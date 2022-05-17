c4c328cc082caf6c32e600b8144b5d5aaf1f1891
Practicante Sistemas (Sebastian Agudelo) <medpracsis03@med.colanta.com>
2022-05-17 12:51:00 -0500

    exportar pdf

---
+++ AdminUserProject/Queue/js/DownloadsPDF.js
@@ -0,0 +1,149 @@
+﻿(function () {
+    $('#create_pdf').on('click', function () {
+        $('body').scrollTop(0);
+        CreatePDFfromHTML();
+    });
+
+    function CreatePDFfromHTML() {
+        var HTML_Width = $("#dashboard").width();
+        var HTML_Height = $("#dashboard").height();
+        var top_left_margin = 15;
+        var PDF_Width = HTML_Width + (top_left_margin * 2);
+        var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+        var canvas_image_width = HTML_Width;
+        var canvas_image_height = HTML_Height;
+
+        var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+        html2canvas($("#dashboard")[0]).then(function (canvas) {
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+            for (var i = 1; i <= totalPDFPages; i++) {
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+            }
+            pdf.save("Resumen del Día - " + moment().format('L') +  ".pdf");
+        });
+    }
+}());
+
+(function () {
+    $('#create_pdf').on('click', function () {
+        $('body').scrollTop(0);
+        CreatePDFfromHTML();
+    });
+
+    function CreatePDFfromHTML() {
+        var HTML_Width = $("#reporteAct").width();
+        var HTML_Height = $("#reporteAct").height();
+        var top_left_margin = 15;
+        var PDF_Width = HTML_Width + (top_left_margin * 2);
+        var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+        var canvas_image_width = HTML_Width;
+        var canvas_image_height = HTML_Height;
+
+        var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+        html2canvas($("#reporteAct")[0]).then(function (canvas) {
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+            for (var i = 1; i <= totalPDFPages; i++) {
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+            }
+            pdf.save("Reporte Actividades - " + moment().format('L') + ".pdf");
+        });
+    }
+}());
+
+(function () {
+    $('#create_pdf').on('click', function () {
+        $('body').scrollTop(0);
+        CreatePDFfromHTML();
+    });
+
+    function CreatePDFfromHTML() {
+        var HTML_Width = $("#softwareReport").width();
+        var HTML_Height = $("#softwareReport").height();
+        var top_left_margin = 15;
+        var PDF_Width = HTML_Width + (top_left_margin * 2);
+        var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+        var canvas_image_width = HTML_Width;
+        var canvas_image_height = HTML_Height;
+
+        var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+        html2canvas($("#softwareReport")[0]).then(function (canvas) {
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+            for (var i = 1; i <= totalPDFPages; i++) {
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+            }
+            pdf.save("Reporte Software - " + moment().format('L') + ".pdf");
+        });
+    }
+}());
+
+(function () {
+    $('#create_pdf').on('click', function () {
+        $('body').scrollTop(0);
+        CreatePDFfromHTML();
+    });
+
+    function CreatePDFfromHTML() {
+        var HTML_Width = $("#hardwareReport").width();
+        var HTML_Height = $("#hardwareReport").height();
+        var top_left_margin = 15;
+        var PDF_Width = HTML_Width + (top_left_margin * 2);
+        var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+        var canvas_image_width = HTML_Width;
+        var canvas_image_height = HTML_Height;
+
+        var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+        html2canvas($("#hardwareReport")[0]).then(function (canvas) {
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+            for (var i = 1; i <= totalPDFPages; i++) {
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+            }
+            pdf.save("Reporte Hardware - " + moment().format('L') + ".pdf");
+        });
+    }
+}());
+
+(function () {
+    $('#create_pdf').on('click', function () {
+        $('body').scrollTop(0);
+        CreatePDFfromHTML();
+    });
+
+    function CreatePDFfromHTML() {
+        var HTML_Width = $("#capturesReport").width();
+        var HTML_Height = $("#capturesReport").height();
+        var top_left_margin = 15;
+        var PDF_Width = HTML_Width + (top_left_margin * 2);
+        var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+        var canvas_image_width = HTML_Width;
+        var canvas_image_height = HTML_Height;
+
+        var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+        html2canvas($("#capturesReport")[0]).then(function (canvas) {
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+            for (var i = 1; i <= totalPDFPages; i++) {
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+            }
+            pdf.save("Reporte de Capturas - " + moment().format('L') + ".pdf");
+        });
+    }
+}());
